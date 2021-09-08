97586f198dbf0ab19a0082c87f781e5035c45bc7
Nari421 <nari3451@gmail.com>
2021-09-08 18:02:04 +0900

    commit

---
+++ BOJ/boj17471.java
@@ -0,0 +1,122 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class boj17471 {
+	static int n,div,min=Integer.MAX_VALUE,population[],c=0;
+	static boolean area[];
+	static ArrayList<Integer>[] connect;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		population = new int[n+1];
+		connect = new ArrayList[n+1];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			population[i] = Integer.parseInt(st.nextToken());
+			connect[i] = new ArrayList<>();
+		}
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int cnt  = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < cnt; j++) {
+				connect[i].add(Integer.parseInt(st.nextToken()));
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			System.out.print(connect[i]+ " :  ");
+			for (int j = 0; j <connect[i].size(); j++) {
+				System.out.print(connect[i].get(j)+" ");
+			}System.out.println();
+		}
+		//부분집합
+		for(int i=0;i<(n/2)+1;i++){
+			div=i;
+			area = new boolean[n+1];
+			combination(1,0);
+		}
+		System.out.println(c);
+		System.out.println(min ==Integer.MAX_VALUE?-1:min);
+		
+
+	}
+	private static void combination(int start,int index) {
+		if(div == index){c++;
+			if(possible()){
+				int sumA=0,sumB=0;
+				for(int i=1;i<=n;i++){
+					if(area[i]) sumA+=population[i];
+					else sumB+=population[i];
+				}
+				min = Math.min(min, Math.abs(sumA-sumB));
+			}
+			return;
+		}
+		for(int i=start;i<n+1;i++){
+			area[i] = true;
+			combination(i+1, index+1);
+			area[i] = false;
+		}
+		
+	}
+	private static boolean possible() {
+		for(int i=1;i<=n;i++){
+			System.out.print(area[i]+" ");
+		}System.out.println();
+		boolean[] visit = new boolean[n+1];
+		int areaA = -1;
+		for(int i=1;i<=n;i++){
+			if(area[i]){
+				areaA = i;
+				break;
+			}
+		}
+		int areaB = -1;
+		for(int i=1;i<=n;i++){
+			if(!area[i]){
+				areaB = i;
+				break;
+			}
+		}
+		if(areaA == -1 || areaB==-1)return false;
+		System.out.println(areaA+" "+areaB);
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(areaA);
+		visit[areaA] = true;
+		while(!q.isEmpty()){
+			int now = q.poll();
+			for (int i = 0; i < connect[now].size(); i++) {
+				if(visit[connect[now].get(i)])continue;
+				if(!area[connect[now].get(i)])continue;
+				visit[connect[now].get(i)] = true;
+				q.add(connect[now].get(i));
+				System.out.println(connect[now].get(i));
+			}
+		}
+		
+		q.add(areaB);
+		visit[areaB] = true;
+		while(!q.isEmpty()){
+			int now = q.poll();
+			for (int i = 0; i < connect[now].size(); i++) {
+				if(visit[connect[now].get(i)])continue;
+				if(area[connect[now].get(i)])continue;
+				visit[connect[now].get(i)] = true;
+				q.add(connect[now].get(i));
+				System.out.println(connect[now].get(i));
+			}
+		}
+		for(int i=1;i<=n;i++){
+			if(!visit[i])return false;
+		}
+		return true;
+	}
+
+}
---
+++ BOJ/boj17780.java
@@ -0,0 +1,124 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class boj17780 {
+	static int n,k,color[][];
+	static int dr[] = {0,0,-1,1};//우 좌 위 아래
+	static int dc[] = {1,-1,0,0};
+	static class Point{
+		int id,r,c,d;
+		public Point(int id, int r,int c,int d){
+			this.id = id;
+			this.r = r;
+			this.c = c;
+			this.d = d;
+		}
+	}
+	//0은 흰색, 1은 빨간색, 2는 파란색
+	static ArrayList<Point> point = new ArrayList<>();
+	static ArrayList<Integer>[][] chess;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		color = new int[n+1][n+1];
+		chess = new ArrayList[n+1][n+1];
+		point.add(new Point(0,0,0,0));
+		for(int i=1;i<=n;i++){
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= n; j++) {
+				color[i][j] = Integer.parseInt(st.nextToken());
+				chess[i][j] = new ArrayList<>();
+			}
+		}
+		for(int i=1;i<=k;i++){
+			st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
+			point.add(new Point(i,r,c,d-1));
+			chess[r][c].add(i);
+		}
+		startGame();
+	}
+	private static void startGame() {
+		int times =0;
+		while(times<=1000){
+			times++;
+			for(int i=1;i<=k;i++){
+				Point p = point.get(i);
+				if(chess[p.r][p.c].indexOf(i) == 0){
+					if(move(i)){
+						System.out.println(times);
+						return;
+					}
+				}
+			}
+		}
+		System.out.println(-1);
+		
+	}
+	private static boolean move(int i) {
+		Point p  = point.get(i);
+		int nr = p.r+dr[p.d];
+		int nc = p.c+dc[p.d];
+		if(nr>n || nc>n || nr<1||nc<1 || color[nr][nc] == 2){
+			if(p.d%2 == 0){
+				point.set(i, new Point(i,p.r,p.c,p.d+1));
+			}else{
+				point.set(i, new Point(i,p.r,p.c,p.d-1));
+			}
+			p  = point.get(i);
+			nr = p.r+dr[p.d];
+			nc = p.c+dc[p.d];
+		}
+		if(nr<=n && nc<=n && nr>=1 && nc>=1 && color[nr][nc] != 2){
+			checkcolor(p,nr,nc);
+			if(chess[nr][nc].size() >=4)return true;
+		}
+			
+		return false;
+	}
+	private static void checkcolor(Point p, int nr, int nc) {
+		ArrayList<Integer> from = chess[p.r][p.c];
+		ArrayList<Integer> to = chess[nr][nc];
+		
+		int stack =from.indexOf(p.id);
+		int fromsize = from.size();
+		switch(color[nr][nc]){
+		case 0:
+			for(int i=stack;i<fromsize;i++){
+				int po = from.get(i);
+				int d = point.get(po).d;
+				point.set(po,new Point(po,nr,nc,d));
+				p = point.get(po);
+				to.add(po);
+			}
+			remove(from,fromsize-1,stack);
+			break;
+		case 1:
+			for(int i=fromsize-1;i>=stack;i--){
+				int po = from.get(i);
+				int d = point.get(po).d;
+				point.set(po,new Point(po,nr,nc,d));
+				p = point.get(po);
+				to.add(po);
+			}
+			remove(from,fromsize-1,stack);
+			break;
+		}
+		
+	}
+	private static void remove(ArrayList<Integer> list, int top, int bottom) {
+		for(int i = top ; i >= bottom ; --i) {
+			list.remove(i);
+		}
+	}
+
+}
